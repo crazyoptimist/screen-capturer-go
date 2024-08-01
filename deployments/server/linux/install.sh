e819a753c68acd290903ff90ce1a52b63f43468b
crazyoptimist <hey@crazyoptimist.net>
2024-08-01 15:34:38 -0500

    fix: linux installation scripts

--- deployments/server/linux/install.sh
+++ deployments/server/linux/install.sh
@@ -2,13 +2,19 @@
 # Abort on error
 set -e
 
+EXE="/usr/local/bin/screen-server-linux"
+REG="${HOME}/.config/systemd/user"
+
 printf 'Have you replaced the vhost name with yours in the service file (y/n)? '
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
   sudo chmod +x ./screen-server-linux
-  sudo cp ./screen-server-linux /usr/local/bin/
-  sudo cp ./screen-capturer.service /etc/systemd/system/
+  sudo cp ./screen-server-linux $EXE
+  mkdir -p $REG
+  cp ./screen-capturer.service "$REG/"
+  systemctl --user enable screen-capturer.service
+  systemctl --user start screen-capturer.service
 
   echo "Successfully installed the application."
 else
